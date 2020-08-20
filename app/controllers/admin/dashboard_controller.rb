4ddafcb91b19c9f9465ed068555e2ba779ffc407
Sara Irving <sarakirving@gmail.com>
2020-08-20 02:45:12 +0000

    creted two instance variables product_count and category_count to pass on to the dashboard view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,8 +4,12 @@ class Admin::DashboardController < ApplicationController
   admin_password = ENV['HTTP_BASIC_PASSWORD']
 
   http_basic_authenticate_with name: admin_name, password: admin_password
+
+
   
   def show
+    @products_count = Product.count
+    @categories_count = Category.count 
   end
 
 end
