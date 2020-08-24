4a1fb9623d8ed93631157faf21a1e96438fa3b5a
Sara Irving <sarakirving@gmail.com>
2020-08-24 23:11:59 +0000

    setup for User model testing and User model authenticat_with_credentials class method testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    
+  end
+
+  describe '.authenticate_with_credentials' do
+    
+  end
+
 end
