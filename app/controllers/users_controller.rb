a5c0a3f0e6fe029611172b58e18e215875bc0c81
Sara Irving <sarakirving@gmail.com>
2020-08-24 22:55:22 +0000

    Define new class method authenticat_with_credentials that will use the authenticate instance method provided by the has_secure_password gem to validate the password given by the user against the encrypted password in the database, if the email and password given match then it will return an instance of that user, if not it will return nil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,23 @@ class UsersController < ApplicationController
     end
   end
 
+  #  Create an instance method authenticat_with_credentials
+  def self.authenticate_with_credentials(params[:email], params[:password])
+
+    # see if there is a user in the database with the given email
+    user = User.find_by_email(params(:email_address)
+
+    # If the user exists AND the password provided is correct then return and instance of that user, if not return nil
+
+    if user && user.authenticate(params(:password)
+      return user
+    else
+      nil
+    end
+
+  end
+
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
