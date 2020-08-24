0b99610a716eb8c1ba6ffbf98864764721cf8bae
Sara Irving <sarakirving@gmail.com>
2020-08-24 22:59:11 +0000

    add minimum length validation to password and passwrod_confirmation, removed it from email where it was placed by mistake

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :password, presence: true
-  validates :password_confirmation, presence: true 
-  validates :email, presence: true,  uniqueness: { case_sensitive: false }, length: {minimum: 5}
+  validates :password, presence: true, length: {minimum: 5}
+  validates :password_confirmation, presence: true, length: {minimum: 5}
+  validates :email, presence: true,  uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
 
