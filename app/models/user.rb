42551cd0387e23fbfeec984325046718ed4e10ae
Sara Irving <sarakirving@gmail.com>
2020-08-24 22:41:20 +0000

    add validation criteria

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  validates :password, presence: true
+  validates :password_confirmation, presence: true 
+  validates :email, presence: true,  uniqueness: { case_sensitive: false }, length: {minimum: 5}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   
 end
