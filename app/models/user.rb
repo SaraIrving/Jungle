5a90aa4c4b65d8d833a903f783e9e088cd0d7c48
Sara Irving <sarakirving@gmail.com>
2020-08-25 00:39:49 +0000

    moved class method definition out of the user controller and into the user model, also removed params[:email] and params[:password] and replaced with simple references to the email and password arguments provided, also used.strip method on the email arugment to remove any accidental whitespace characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,21 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+
+  #  Create an class method authenticate_with_credentials
+  def self.authenticate_with_credentials(email_address, password)
+
+    # see if there is a user in the database with the given email
+    user = User.find_by_email(email_address.strip)
+
+    # If the user exists AND the password provided is correct then return and instance of that user, if not return nil
+
+    if user && user.authenticate(password)
+      return user
+    else
+      nil
+    end
+
+  end
   
 end
