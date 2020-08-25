311e16b4c47fe9fa13e5d0f0df92513450a75d8e
Sara Irving <sarakirving@gmail.com>
2020-08-25 00:41:08 +0000

    removed authenticate_with_credentials class method definition and moved it to the user model instead

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,15 +12,15 @@ class UsersController < ApplicationController
     end
   end
 
-  #  Create an instance method authenticat_with_credentials
-  def self.authenticate_with_credentials(params[:email], params[:password])
+  #  Create an class method authenticat_with_credentials
+  def self.authenticate_with_credentials(email_address, password)
 
     # see if there is a user in the database with the given email
-    user = User.find_by_email(params(:email_address)
+    user = User.find_by_email(email_address)
 
     # If the user exists AND the password provided is correct then return and instance of that user, if not return nil
 
-    if user && user.authenticate(params(:password)
+    if user && user.authenticate(password)
       return user
     else
       nil
